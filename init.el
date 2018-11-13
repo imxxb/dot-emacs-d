c1de721dbccfcaf9a5017312e24a9ee5120a5bbd
imxxb <imxxb@qq.com>
2018-11-13 20:10:22 +0800

    add install package function

--- init.el
+++ init.el
@@ -1,22 +1,40 @@
+(require 'package)
+
 (setq package-archives '(("gnu"   . "http://mirrors.tuna.tsinghua.edu.cn/elpa/gnu/")
                          ("melpa" . "http://mirrors.tuna.tsinghua.edu.cn/elpa/melpa/")))
 (package-initialize)
+(require 'cl)
+
+(defvar imxxb/packages '(
+			 evil
+			 htmlize
+			 )
+  "Default packages")
+
+(setq package-selected-packages imxxb/packages)
+
+;;(defun imxxb/packages-installed-p ()
+;;  (loop for pkg in imxxb/packages
+;;	when
+;;	(not (package-installed-p pkg))
+;;	do
+;;	(return nil)
+;;	finally (return t)))
+
+;;(unless (imxxb/packages-installed-p)
+;;  (message "%s" "Refreshing package database...")
+;;  (package-refresh-contents)
+;;  (dolist (pkg imxxb/packages)
+;;    (when (not (package-installed-p pkg))
+;;      (package-install pkg))))
+(dolist (pkg imxxb/packages)
+  (when (not (package-installed-p pkg))
+  (package-install pkg)
+  ))
+
 
 (set-default-font "Inconsolata-12")
 (global-set-key "\C-ca" 'org-agenda)
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (evil htmlize))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
-
 (toggle-truncate-lines 1)
 
 (defun open-init-file()
