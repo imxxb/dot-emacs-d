e77c273d0827ecf3469b6652b4abb817de6c7e9f
imxxb <imxxb@qq.com>
2018-11-13 17:53:52 +0800

    init project

---
+++ init.el
@@ -0,0 +1,22 @@
+(setq package-archives '(("gnu"   . "http://mirrors.tuna.tsinghua.edu.cn/elpa/gnu/")
+                         ("melpa" . "http://mirrors.tuna.tsinghua.edu.cn/elpa/melpa/")))
+(package-initialize)
+
+(set-default-font "Inconsolata-12")
+(global-set-key "\C-ca" 'org-agenda)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages (quote (evil htmlize))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+(evil-mode 1)
+(toggle-truncate-lines 1)
+
+
