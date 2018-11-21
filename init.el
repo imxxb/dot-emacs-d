66118c4c0bad10c3a9fa4665b5e42afd3ef3045b
imxxb <imxxb@qq.com>
2018-11-21 13:45:29 +0800

    use org babel

--- init.el
+++ init.el
@@ -3,6 +3,10 @@
 (require 'init-packages)
 (require 'init-better-defaults)
 
+(require 'org-install)
+(require 'ob-tangle)
+(org-babel-load-file (expand-file-name "configuration.org" user-emacs-directory))
+
 
 (package-refresh-contents)
 (package-install-selected-packages)
