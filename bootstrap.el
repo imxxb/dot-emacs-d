da594fe0a2ec8879e1d24af9975ebf2d185e44dd
imxxb <imxxb@qq.com>
2018-11-25 21:48:56 +0800

    refactor with org bable

---
+++ bootstrap.el
@@ -0,0 +1,4 @@
+(require 'org-install)
+(require 'ob-tangle)
+
+(org-babel-load-file (expand-file-name "configuration.org" user-emacs-directory))
--- lisp/init-better-defaults.el
+++
@@ -1,6 +0,0 @@
-(toggle-truncate-lines 1)
-
-(provide 'init-better-defaults)
-
-;;怎样自动加载buff？
-(global-auto-revert-mode 1)
