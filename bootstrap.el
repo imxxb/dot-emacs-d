c627abdbb3a45490b5075ce9036d20ace58f9f3f
imxxb <imxxb@qq.com>
2018-12-02 08:55:47 +0800

    tangle to dest directory

--- bootstrap.el
+++ bootstrap.el
@@ -1,5 +1,5 @@
 (require 'org-install)
 (require 'ob-tangle)
 
-;; (org-babel-load-file (expand-file-name "configuration.org" user-emacs-directory))
+;; (org-babel-load-file (expand-file-name "better-defaults-for-emacs.org" user-emacs-directory))
 (org-babel-load-file (expand-file-name "how-to-build-your-own-spacemacs.org" user-emacs-directory))
